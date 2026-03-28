3c29325646623ff574b807cab748f0297083bade
mplaczek99 <mplaczek99@gmail.com>
2026-03-28 00:44:52 -0500

    add basic image preview in GUI

--- cmd/xrayview-gui/main.go
+++ cmd/xrayview-gui/main.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"fmt"
+	"image"
+	_ "image/jpeg"
+	_ "image/png"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/dialog"
 	"fyne.io/fyne/v2/widget"
@@ -14,18 +18,36 @@ func main() {
 	a := app.New()
 	w := a.NewWindow("xrayview")
 	pathLabel := widget.NewLabel("No image selected")
+	preview := canvas.NewImageFromImage(nil)
+	preview.FillMode = canvas.ImageFillContain
+	preview.SetMinSize(fyne.NewSize(320, 240))
+	preview.Hide()
 	w.SetContent(container.NewVBox(
 		widget.NewLabel("xrayview GUI starting"),
 		pathLabel,
+		preview,
 		widget.NewButton("Open Image", func() {
 			dialog.ShowFileOpen(func(reader fyne.URIReadCloser, err error) {
-				if err != nil || reader == nil {
+				if err != nil {
+					dialog.ShowError(err, w)
 					return
 				}
+				if reader == nil {
+					return
+				}
+				defer reader.Close()
+
+				if _, _, err := image.DecodeConfig(reader); err != nil {
+					dialog.ShowError(err, w)
+					return
+				}
+
 				path := reader.URI().Path()
 				fmt.Println(path)
 				pathLabel.SetText(path)
-				reader.Close()
+				preview.File = path
+				preview.Show()
+				preview.Refresh()
 			}, w)
 		}),
 	))
